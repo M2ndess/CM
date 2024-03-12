ca4ffac543e2539bdbf2e0eee137869abb08bcbe
M2ndess <121449441+M2ndess@users.noreply.github.com>
2024-03-12 17:19:47 +0000

    aa

---
+++ app/src/main/java/com/example/a001_primeiraappandroid/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.a001_primeiraappandroid
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun convertCurrency(view: View?) {
+        val dollarText = findViewById<EditText>(R.id.dollarText)
+        val textView = findViewById<TextView>(R.id.textView)
+        if (dollarText.text.toString() != "") {
+            val dollarValue = dollarText.text.toString().toFloat()
+            val euroValue = dollarValue * 0.85f
+            textView.text = String.format(Locale.ENGLISH, "%.2f", euroValue)
+        } else {
+            textView.setText(R.string.no_value_string)
+        }
+    }
+}
